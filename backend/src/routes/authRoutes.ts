edb80482cd6f2318598ac9f807ede87fa3e062ce
hugo <86883067+programador-cyberpunk@users.noreply.github.com>
2026-06-28 20:27:22 -0300

    incrementação
    Eu incrementai o authRotes, middlewares/auth e authController, porem ainda tem alguns erros de codigo que eu to tentando resolver, não sei se foi erro meu de lógica, ou se falta algo no meu ambiente pra rodar tudo certinho

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,11 +1,17 @@
 import { Router } from 'express';
-import { register, login, me } from '../controllers/authController';
-import { authenticate } from '../middlewares/auth';
+import { login, ME, register } from '../controllers/authController';
+import { adminMiddleware, authMiddleWare } from '../middlewares/auth';
 ///mudei o nome pra nao me confundir
 const router = Router();
 
+//rotas publicas e autenticadas
 router.post('/register', register);
 router.post('/login', login);
-router.get('/me', authenticate, me);
+router.get('/me', authMiddleWare, ME);
+
+//rota protegida
+router.delete('/admin/deletar-reserva/:id', authMiddleWare, adminMiddleware, (req, res) => {
+    res.json({ message: "Reserva deletada com sucesso por um ADMIN." });
+});
 
 export default router;
